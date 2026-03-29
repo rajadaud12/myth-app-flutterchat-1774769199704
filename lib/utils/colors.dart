d408f90c7e6ba426673f5cbd862751fbe9c81eb6
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-29 12:27:03 +0500

    Update lib/utils/colors.dart via MYTH Build System

---
+++ lib/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = Color(0xFF075E54);
+  static Color primaryLight = Color(0xFF128C7E);
+  static Color primaryDark = Color(0xFF055249);
+  static Color accent = Color(0xFF25D366);
+  static Color secondaryAccent = Color(0xFF34B7F1);
+  static Color background = Color(0xFFEDEDED);
+  static Color chatBubble = Color(0xFFDCF8C6);
+  static Color white = Color(0xFFFFFFFF);
+  static Color black = Color(0xFF000000);
+  static Color grey = Color(0xFF9E9E9E);
+  static Color lightGrey = Color(0xFFE8E8E8);
+  static Color darkGrey = Color(0xFF303030);
+  static Color online = Color(0xFF25D366);
+  static Color unread = Color(0xFF53Bdeb);
+  static Color red = Color(0xFFFF5252);
+  static Color messageBlue = Color(0xFFEEF5FB);
+}
