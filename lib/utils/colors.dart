fad50ed1a63e75285e4cabd529dc7966c6047d14
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-29 12:36:30 +0500

    Update lib/utils/colors.dart via MYTH Build System

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,15 +6,22 @@ class AppColors {
   static Color primaryDark = Color(0xFF055249);
   static Color accent = Color(0xFF25D366);
   static Color secondaryAccent = Color(0xFF34B7F1);
-  static Color background = Color(0xFFEDEDED);
+  static Color background = Color(0xFFF7F8FA);
+  static Color surface = Color(0xFFFFFFFF);
   static Color chatBubble = Color(0xFFDCF8C6);
+  static Color chatBubbleOther = Color(0xFFFFFFFF);
   static Color white = Color(0xFFFFFFFF);
   static Color black = Color(0xFF000000);
-  static Color grey = Color(0xFF9E9E9E);
+  static Color grey = Color(0xFF667781);
   static Color lightGrey = Color(0xFFE8E8E8);
   static Color darkGrey = Color(0xFF303030);
   static Color online = Color(0xFF25D366);
-  static Color unread = Color(0xFF53Bdeb);
+  static Color unread = Color(0xFF53BDEB);
   static Color red = Color(0xFFFF5252);
   static Color messageBlue = Color(0xFFEEF5FB);
+  static Color divider = Color(0xFFE8E8E8);
+  static Color cardShadow = Color(0x0A000000);
+  static Color textPrimary = Color(0xFF111B21);
+  static Color textSecondary = Color(0xFF667781);
+  static Color inputBackground = Color(0xFFF0F2F5);
 }
